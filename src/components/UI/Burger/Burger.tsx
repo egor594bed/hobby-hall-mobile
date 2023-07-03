5bab540b7ec1e6e10b2ecb39ee7972f8b7278a20
egor594bed <egor594work@gmail.com>
2023-07-04 00:15:04 +0300

    burger anim

--- src/components/UI/Burger/Burger.tsx
+++ src/components/UI/Burger/Burger.tsx
@@ -1,19 +1,87 @@
-import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useEffect } from 'react';
 import { colors } from '../../../assets/style/_colors';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 import { toggleBurger } from '../../../redux/slices/catalog';
+import { RootState } from '../../../redux/store';
 
 export const Burger = () => {
   const dispatch = useDispatch();
+  const burgerIsOpen = useSelector(
+    (state: RootState) => state.catalogSlice.burgerState,
+  );
+  const rotate = useSharedValue('0deg');
+  const translate = useSharedValue(0);
+  const opacity = useSharedValue(1);
+
+  //разобраться с TS
+
+  const topSliceAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: withSpring(rotate.value),
+        },
+        { translateY: withSpring(translate.value) },
+      ],
+    };
+  });
+  const centerSliceAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      opacity: withSpring(opacity.value),
+    };
+  });
+  const bottomSliceAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: withSpring('-' + rotate.value),
+        },
+        { translateY: withSpring(Number('-' + translate.value)) },
+      ],
+    };
+  });
+
+  useEffect(() => {
+    if (burgerIsOpen) {
+      rotate.value = '45deg';
+      opacity.value = 0;
+      translate.value = 11;
+    } else {
+      rotate.value = '0';
+      opacity.value = 1;
+      translate.value = 0;
+    }
+  }, [burgerIsOpen]);
+
   return (
-    <>
-      <Ionicons
-        name="reorder-three-outline"
-        color={colors.firstColor}
-        size={50}
-        onPress={() => dispatch(toggleBurger())}
-      />
-    </>
+    <TouchableWithoutFeedback onPress={() => dispatch(toggleBurger())}>
+      <View style={styles.burder}>
+        <Animated.View style={[styles.burderSlice, topSliceAnimatedStyles]} />
+        <Animated.View
+          style={[styles.burderSlice, centerSliceAnimatedStyles]}
+        />
+        <Animated.View
+          style={[styles.burderSlice, bottomSliceAnimatedStyles]}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  burder: {
+    flexDirection: 'column',
+    gap: 5,
+  },
+  burderSlice: {
+    width: 40,
+    height: 3,
+    backgroundColor: colors.firstColor,
+  },
+});
